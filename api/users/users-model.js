f7c2bcd323cd4fc6cbfcd781fd22b9d53dd4bcc9
ibrahimfevzi <ibrahimfevzi@gmail.com>
2023-05-30 19:19:46 +0300

    tamamlandı

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,29 +1,41 @@
+const db = require("../../data/db-config");
+
 /**
+
   tüm kullanıcıları içeren bir DİZİ ye çözümlenir, tüm kullanıcılar { user_id, username } içerir
  */
 function bul() {
-
+  return db("users");
 }
 
 /**
   verilen filtreye sahip tüm kullanıcıları içeren bir DİZİ ye çözümlenir
  */
 function goreBul(filtre) {
-
+  return db("users").where(filtre);
 }
 
 /**
   verilen user_id li kullanıcıya çözümlenir, kullanıcı { user_id, username } içerir
  */
-function idyeGoreBul(user_id) {
-
+async function idyeGoreBul(user_id) {
+  let objOrn = await db("users").where("user_id", user_id).first();
+  return objOrn;
 }
 
 /**
   yeni eklenen kullanıcıya çözümlenir { user_id, username }
  */
-function ekle(user) {
-
+async function ekle(user) {
+  let [user_id] = await db("users").insert(user);
+  return idyeGoreBul(user_id);
 }
 
 // Diğer modüllerde kullanılabilmesi için fonksiyonları "exports" nesnesine eklemeyi unutmayın.
+
+module.exports = {
+  bul,
+  goreBul,
+  idyeGoreBul,
+  ekle,
+};
